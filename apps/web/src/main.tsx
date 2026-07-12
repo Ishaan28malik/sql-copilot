2dcd084f2dcb3477748dfa83997d9e9e55995120
Ishaan28malik <ishaanmalikrm7@gmail.com>
2026-07-12 11:31:50 +0530

    Support hosting under a sub-path (GitHub Pages)

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
